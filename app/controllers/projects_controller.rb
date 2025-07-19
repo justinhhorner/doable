1d54d57fe16eab0e19e0322b74b0ea3893e62a8e
Justin Horner <justinh.horner@gmail.com>
2025-07-19 11:59:22 -0400

    First

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,51 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new 
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    
+    if @project.save
+      flash[:notice] = "Project created succesfully!"
+      redirect_to project_path(@project)
+    else
+      render:new, status: :unprocessable_entity
+    end
+  end
+
+  def edit 
+    @project = Project.find(params[:id])
+  end
+  
+  def update 
+    @project = Project.find(params[:id])
+    
+    if @project.update(project_params)
+      flash[:notice] = "Project updated succesfully!"
+      redirect_to project_path(@project)
+    else
+      render:new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    @project = Project.find(params[:id])
+
+    @project.destroy
+    flash[:notice] = "Project deleted."
+    redirect_to projects_path
+  end
+
+  def project_params
+    params.expect(project: [:name])
+  end
+end
